14d975dd6db91dae6dc50806fd7f775dfbbac2f0
Nazam <nazam98-be@email.com>
2020-01-25 15:27:33 +0100

    header responsive done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,22 +4,21 @@ const Nav = () => {
 
     return (
 
-        <nav className="navbar navbar-expand-lg navbar-light bg-transparent container mx-auto">
-            <img src={Logo} alt="" className='img-fluid w-25 col-1'/>
+        <nav className="navbar navbar-expand-lg navbar-light bg-transparent mx-auto">
+            <img src={Logo} alt="" className='img-fluid w-25 col-lg-1' />
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav offset-lg-3 col-lg-6 text-center">
 
-                    <a className="col-lg col  nav-item nav-link active" href="#">Home<span className="sr-only">(current)</span></a>
+                    <a className="col-lg col nav-item nav-link active" href="#">Home<span className="sr-only">(current)</span></a>
                     <a className="col-lg col nav-item nav-link" href="#">About</a>
                     <a className="col-lg col nav-item nav-link" href="#">Services</a>
                     <a className="col-lg col nav-item nav-link" href="#">Blog</a>
                     <a className="col-lg col nav-item nav-link" href="#">Pages</a>
                     <a className="col-lg col nav-item nav-link" href="#">Contact</a>
-
-                    <button className='btn btn-primary w-25 col-2 offset-2 rounded-0'>Contact me</button>
+                    <button className='btn text-white w-25 col-lg-2 col-2 offset-lg-2 rounded-0'>Contact me</button>
                 </div>
             </div>
         </nav>
