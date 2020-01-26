cb944bd4e9172a63bf50b3c5920d30af9911fc05
Nazam <nazam98-be@email.com>
2020-01-26 12:46:46 +0100

    Section 1 corrected

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -18,17 +18,17 @@ const Section1 = () => {
             </div>
             <div className="container my-5 mx-auto">
                 <div className="row">
-                    <div className="col-lg col-8 p-5  mx-4 my-3 border">
+                    <div className="col-lg col-8 p-5  mx-4 my-3 service">
                         <img className='img-fluid' src={Graphic} alt="" />
                         <h4 className='my-4'>Graphic design</h4>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur itaque nesciunt </p>
                     </div>
-                    <div className="col-lg col-8 p-5  mx-4 my-3 border">
+                    <div className="col-lg col-8 p-5  mx-4 my-3 service">
                         <img className='img-fluid' src={Web} alt="" />
                         <h4 className='my-4'>Web design</h4>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur itaque nesciunt </p>
                     </div>
-                    <div className="col-lg col-8 p-5  mx-4 my-3 border">
+                    <div className="col-lg col-8 p-5  mx-4 my-3 service">
                         <img className='img-fluid' src={Mobile} alt="" />
                         <h4 className='my-4'>Mobile app</h4>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur itaque nesciunt </p>
